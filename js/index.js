fe498ffc5f682e9491fd022e1ab48304f0392d18
No1RajAryan <55110091+No1RajAryan@users.noreply.github.com>
2020-04-23 15:47:54 +0530

    First website pload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+
+
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myList li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
